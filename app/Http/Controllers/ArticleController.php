cbb885a069d1b89f115552d7c33b397c34ee6c54
annakarpova <anna.karpova00@yandex.ru>
2024-01-30 16:53:35 +0300

    forms added

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Article;
+use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
@@ -11,9 +12,29 @@ class ArticleController extends Controller
         return view('article.index', ['articles' => $articles]);
     }
 
+    public function create()
+    {
+        $article = new Article();
+        return view ('article.create', compact('article'));
+    }
+
+    public function store (Request $request)
+    {
+        $data = $this->validate($request, [
+            'name'=>'required|unique:articles',
+            'body'=>'required|min:1000'
+        ]);
+        $article = new Article();
+        $article->fill($data);
+        $article->save();
+        return redirect()->route('articles.index');
+    }
+
     public function show($id)
     {
         $article = Article::findOrFail($id);
         return view('article.show', compact('article'));
     }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/', [PageController::class, 'index'])->name ('page.index');
 Route::get('articles', [ArticleController::class, 'index'])
     ->name('articles.index');
 
+Route::get('articles/create', [ArticleController::class, 'create'])->name('article.create');
+Route::post('articles', [ArticleController::class, 'store'])->name('articles.store');
+
 Route::get('articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
+
 
 
